23a4b41e392f24e5c1aeb5d7d4b651a56369f38d
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-05-30 10:48:47 +0900

    29.Class Validator

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,18 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNotEmpty, MaxLength, Min, IsString } from 'class-validator';
+
 export class CreateItemDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(40)
     name: string;
+
+    @IsInt()
+    @Min(1)
+    @Type(() => Number)
     price: number;
+
+    @IsString()
+    @IsNotEmpty()
     description: string;
 }
